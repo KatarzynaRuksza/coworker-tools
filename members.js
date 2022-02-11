84a9467876803ae7d32769d8bb980a398d2461af
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-02-11 18:35:18 +0000

    Update members.js

--- members.js
+++ members.js
@@ -1,3 +1,48 @@
-const members = [];
+const names = [
+    {
+        firsName: 'John',
+        lastName: 'Doe',
+        jobTitle: 'Software Engineer',
+        department: 'Engineering',
+        manager: 'null',
+        location: 'Glasgow',
+        firstAider: 'No',
+        userSlack: '.johndoe',
+        timeZone: 'America/Los_Angeles'
+    },
+    {
+        firsName: 'Jane',
+        lastName: 'Doe',
+        jobTitle: 'Software Engineer',
+        department: 'Engineering',
+        manager: 'John Doe',
+        location: 'Leeds',
+        firstAider: 'No',
+        userSlack: '.janedoe',
+        timeZone: 'America/Los_Angeles'
+    },
+    {
+        firsName: 'Bob',
+        lastName: 'Doe',
+        jobTitle: 'Software Engineer',
+        department: 'Engineering',
+        manager: 'Jane Doe',
+        location: 'Cape Town',
+        firstAider: 'No',
+        userSlack: '.bobdoe',
+        timeZone: 'America/Los_Angeles'
+    },
+    {
+        firsName: 'Sally',
+        lastName: 'Doe',
+        jobTitle: 'Software Engineer',
+        department: 'Engineering',
+        manager: 'Jane Doe',
+        location: 'Rome',
+        firstAider: 'No',
+        userSlack: '.sallydoe',
+        timeZone: 'America/Los_Angeles'
+    }
+]
 
 module.exports = members;
